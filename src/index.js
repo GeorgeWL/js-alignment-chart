a4dd6fbef2f83983b04aa6760916978d4070dd1d
George W Langham <georgelangham1990@gmail.com>
2021-12-12 23:05:56 +0000

    header and footer add

--- src/index.js
+++ src/index.js
@@ -1,15 +1,7 @@
 import "./styles.css";
 
-// document.getElementById("app").innerHTML = `
-// <h1>Hello Vanilla!</h1>
-// <div>
-//   We use the same configuration as Parcel to bundle this sandbox, you can find more
-//   info about Parcel
-//   <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-// </div>
-// `;
 const inputsArray = Array.from(document.querySelectorAll("input[type=file]"));
-console.log(inputsArray);
+
 function updateImageForInput({ target: { id } }) {
   console.log({ id });
 }
